522c970dcae73e699d32f60c4b223e8fedcf3e81
Solomiia Leshchuk <solosun0308@gmail.com>
2026-05-08 15:55:42 +0300

    Added code to Your First Machine Learning Model

--- Intro to Machine Learning/Your First Machine Learning Model/code.py
+++ Intro to Machine Learning/Your First Machine Learning Model/code.py
@@ -1,1 +1,28 @@
+import pandas as pd
+
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex3 import *
+
+print("Setup Complete")
+
+print(home_data.columns)
+
+y = home_data['SalePrice']
+
+feature_names = ["LotArea", "YearBuilt", "1stFlrSF", "2ndFlrSF",
+                      "FullBath", "BedroomAbvGr", "TotRmsAbvGrd"]
+
+X=home_data[feature_names]
+
+from sklearn.tree import DecisionTreeRegressor
+iowa_model = DecisionTreeRegressor(random_state=1)
+iowa_model.fit(X, y)
+
+predictions = iowa_model.predict(X)
+print(predictions)
 
