5133576451f354ff7b66850705490d050e7286d9
Solomiia Leshchuk <solosun0308@gmail.com>
2026-05-08 15:58:48 +0300

    Added code to Model Validation(Intro to machine learning)

--- Intro to Machine Learning/Model Validation/code.py
+++ Intro to Machine Learning/Model Validation/code.py
@@ -1,1 +1,33 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[feature_columns]
+
+iowa_model = DecisionTreeRegressor()
+iowa_model.fit(X, y)
+
+print("First in-sample predictions:", iowa_model.predict(X.head()))
+print("Actual target values for those homes:", y.head().tolist())
+
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex4 import *
+print("Setup Complete")
+
+from sklearn.model_selection import train_test_split
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+iowa_model = DecisionTreeRegressor(random_state=1)
+iowa_model.fit(train_X, train_y)
+
+val_predictions = iowa_model.predict(val_X)
+
+print(val_predictions[:5])
+
+print(val_y.head())
 
