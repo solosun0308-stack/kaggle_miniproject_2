8531c72a6d5d1b4f8fb03323aa8853548bf6c0bd
Solomiia Leshchuk <solosun0308@gmail.com>
2026-05-08 15:52:40 +0300

    Added code to Basic data exploration

--- Intro to Machine Learning/Basic Data Exploration/code.py
+++ Intro to Machine Learning/Basic Data Exploration/code.py
@@ -1,1 +1,13 @@
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex2 import *
+print("Setup Complete")
 
+import pandas as pd
+
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+
+avg_lot_size = 10517
+newest_home_age = 16
