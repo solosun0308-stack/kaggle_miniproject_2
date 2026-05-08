e946ef0d2acdf7286dc37088f9a80f2aa53da236
Solomiia Leshchuk <solosun0308@gmail.com>
2026-05-08 16:04:54 +0300

    Added code to Random Forests(Intro to machine learning)

--- Intro to Machine Learning/Random Forests/code.py
+++ Intro to Machine Learning/Random Forests/code.py
@@ -1,1 +1,44 @@
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
+
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+iowa_model = DecisionTreeRegressor(random_state=1)
+iowa_model.fit(train_X, train_y)
+
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".format(val_mae))
+
+iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".format(val_mae))
+
+
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex6 import *
+print("\nSetup complete")
+
+from sklearn.ensemble import RandomForestRegressor
+
+rf_model = RandomForestRegressor()
+
+rf_model.fit(train_X, train_y)
+
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+
+print("Validation MAE for Random Forest Model: {}".format(rf_val_mae))
