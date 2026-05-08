eb39b587dc84aa9cdee35cada175a9866615f28d
Solomiia Leshchuk <solosun0308@gmail.com>
2026-05-08 17:11:25 +0300

    added code to Partial Plots(machine learning explainability)

--- Machine Learning Explainability/Partial Plots/code.py
+++ Machine Learning Explainability/Partial Plots/code.py
@@ -1,1 +1,124 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_explainability.ex3 import *
+print("Setup Complete")
+
+data = pd.read_csv('../input/new-york-city-taxi-fare-prediction/train.csv', nrows=50000)
+
+data = data.query('pickup_latitude > 40.7 and pickup_latitude < 40.8 and ' +
+                  'dropoff_latitude > 40.7 and dropoff_latitude < 40.8 and ' +
+                  'pickup_longitude > -74 and pickup_longitude < -73.9 and ' +
+                  'dropoff_longitude > -74 and dropoff_longitude < -73.9 and ' +
+                  'fare_amount > 0'
+                  )
+
+y = data.fare_amount
+
+base_features = ['pickup_longitude',
+                 'pickup_latitude',
+                 'dropoff_longitude',
+                 'dropoff_latitude']
+
+X = data[base_features]
+
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+first_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(train_X, train_y)
+print("Data sample:")
+data.head()
+
+data.describe()
+
+from matplotlib import pyplot as plt
+from sklearn.inspection import PartialDependenceDisplay
+
+feat_name = 'pickup_longitude'
+PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+plt.show()
+
+for feat_name in base_features:
+    PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+
+    plt.show()
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+fnames = [('pickup_longitude', 'dropoff_longitude')]
+disp = PartialDependenceDisplay.from_estimator(first_model, val_X, fnames, ax=ax)
+plt.show()
+
+savings_from_shorter_trip = 6
+
+feat_name = 'pickup_longitude'
+PartialDependenceDisplay.from_estimator(first_model, val_X, [feat_name])
+plt.show()
+
+data['abs_lon_change'] = abs(data.dropoff_longitude - data.pickup_longitude)
+data['abs_lat_change'] = abs(data.dropoff_latitude - data.pickup_latitude)
+
+features_2  = ['pickup_longitude',
+               'pickup_latitude',
+               'dropoff_longitude',
+               'dropoff_latitude',
+               'abs_lat_change',
+               'abs_lon_change']
+
+X = data[features_2]
+new_train_X, new_val_X, new_train_y, new_val_y = train_test_split(X, y, random_state=1)
+second_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(new_train_X, new_train_y)
+
+feat_name = 'pickup_longitude'
+disp = PartialDependenceDisplay.from_estimator(second_model, new_val_X, [feat_name])
+plt.show()
+
+import numpy as np
+import pandas as pd
+from numpy.random import rand
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.inspection import PartialDependenceDisplay
+import matplotlib.pyplot as plt
+
+n_samples = 20000
+
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+
+y = -2 * X1 * (X1<-1) + X1 - 2 * X1 * (X1>1) - X2
+
+
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'])
+plt.show()
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+n_samples = 20000
+
+X1 = 4 * rand(n_samples) - 2
+X2 = 4 * rand(n_samples) - 2
+y = X1 * X2
+
+
+
+my_df = pd.DataFrame({'X1': X1, 'X2': X2, 'y': y})
+predictors_df = my_df.drop(['y'], axis=1)
+
+my_model = RandomForestRegressor(n_estimators=30, random_state=1).fit(predictors_df, my_df.y)
+
+
+disp = PartialDependenceDisplay.from_estimator(my_model, predictors_df, ['X1'], grid_resolution=300)
+plt.show()
+
+perm = PermutationImportance(my_model).fit(predictors_df, my_df.y)
+
+
+eli5.show_weights(perm, feature_names = ['X1', 'X2'])
